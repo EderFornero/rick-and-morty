2523eb34e93db7084898f174d16a270543ac4eb2
eder <ederfornero.ef@gmail.com>
2025-03-04 20:15:36 -0300

    hashed routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,34 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { createHashRouter, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Characters from './routes/Characters.tsx'
+import Locations from './routes/Locations.tsx'
+import Episodes from './routes/Episodes.tsx'
 import App from './App.tsx'
+
+
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/characters',
+    element: <Characters />
+  },
+  {
+    path: '/locations',
+    element: <Locations />
+  },
+  {
+    path: '/episodes',
+    element: <Episodes />
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
