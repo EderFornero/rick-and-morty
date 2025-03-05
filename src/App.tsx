1ab35b031632ab9cd236ea54b47ea0430594b9c4
eder <ederfornero.ef@gmail.com>
2025-03-04 21:05:21 -0300

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,32 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import { getApi } from './utils/getApi'
 import styled from 'styled-components'
+import Loader from './components/Loader/Loader'
+import { Link } from 'react-router-dom'
 
 function App() {
 
   const [apiData, setApiData] = useState<string>("")
+  const [loading, setLoading] = useState<boolean>(false)
 
   useEffect(() => {
     const fetchApi = async () => {
-      const data = await getApi();
-      if (data) {
-        console.log("DATA", data)
-        setApiData(data)
-      } else {
-        console.log("Error charging data.")
+      try {
+        setLoading(true)
+        const data = await getApi();
+
+        if (!data) throw new Error("Error fetching data in front.")
+
+        if (data) {
+          //console.log("DATA", data)
+          setApiData(data)
+          setLoading(false)
+        }
+      } catch (error) {
+        console.log(error)
+        setLoading(false)
+      } finally {
+        setLoading(false)
       }
     }
 
@@ -29,20 +42,42 @@ function App() {
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
         <h1>RICK AND MORTY</h1>
       </div>
-      <Div>
-        <MainCard>
-          <img width={250} height={200} alt='Characters' src='https://rickandmortyapi.com/api/character/avatar/1.jpeg' />
-          <h3>Characters</h3>
-        </MainCard>
-        <MainCard>
-          <img width={250} height={200} alt='Locations' src='https://www.looper.com/img/gallery/the-most-disturbing-dimension-ever-visited-on-rick-and-morty/the-dimension-known-as-buttworld-1573075311.jpg' />
-          <h3>Locations</h3>
-        </MainCard>
-        <MainCard>
-          <img width={250} height={200} alt='Episodes' src='https://upload.wikimedia.org/wikipedia/en/b/b8/Rick_and_Morty_season_1.png' />
-          <h3>Episodes</h3>
-        </MainCard>
-      </Div>
+      {
+        apiData
+          ? <Div>
+            <Link to="/characters">
+              <MainCard>
+                <img width={250} height={200} alt='Characters' src='https://rickandmortyapi.com/api/character/avatar/1.jpeg' />
+                <h3>
+                  Characters
+                </h3>
+              </MainCard>
+            </Link>
+
+            <Link to="/locations">
+              <MainCard>
+                <img width={250} height={200} alt='Locations' src='https://www.looper.com/img/gallery/the-most-disturbing-dimension-ever-visited-on-rick-and-morty/the-dimension-known-as-buttworld-1573075311.jpg' />
+                <h3>
+                  Locations
+                </h3>
+              </MainCard>
+            </Link>
+
+            <Link to="/episodes">
+              <MainCard>
+                <img width={250} height={200} alt='Episodes' src='https://upload.wikimedia.org/wikipedia/en/b/b8/Rick_and_Morty_season_1.png' />
+                <h3>Episodes</h3>
+              </MainCard>
+            </Link>
+
+          </Div>
+          :
+          <div style={{ width: '100%', justifyContent: 'center', alignItems: 'center' }}>
+            <Loader />
+          </div>
+
+      }
+
     </>
   )
 }
