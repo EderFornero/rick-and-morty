d5394860ffb9af4a77879263906d64398e8e908f
eder <ederfornero.ef@gmail.com>
2025-03-04 21:11:44 -0300

    get api urls

--- src/utils/getApi.ts
+++ src/utils/getApi.ts
@@ -1,4 +1,4 @@
-export const getApi = async () => {
+const getApi = async () => {
   try {
     const response = await fetch("https://rickandmortyapi.com/api");
     if (!response.ok) throw new Error("Error fetching API.");
@@ -11,3 +11,44 @@ export const getApi = async () => {
     return null;
   }
 };
+
+const getChars = async () => {
+  try {
+    const response = await fetch("https://rickandmortyapi.com/api/character");
+
+    if (!response.ok) throw new Error("Error fetching characters url.");
+
+    const data = response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getLocations = async () => {
+  try {
+    const response = await fetch("https://rickandmortyapi.com/api/location");
+
+    if (!response.ok) throw new Error("Error fetching locations url.");
+
+    const data = response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getEpisodes = async () => {
+  try {
+    const response = await fetch("https://rickandmortyapi.com/api/episode");
+
+    if (!response.ok) throw new Error("Error fetching episodes url.");
+
+    const data = response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { getApi, getChars, getLocations, getEpisodes };
